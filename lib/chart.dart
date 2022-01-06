9c37b69b88c17ab3c1bd1d016c28c92d11808844
nikondrat <nikondratoriginal@gmail.com>
2022-01-06 13:54:13 +0300

    main

--- lib/chart.dart
+++ lib/chart.dart
@@ -7,90 +7,95 @@ class FinanceChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BarChart(BarChartData(
-        maxY: 10000,
-        borderData: FlBorderData(show: false),
-        gridData: FlGridData(show: false),
-        titlesData: FlTitlesData(
-            bottomTitles: SideTitles(showTitles: false),
-            leftTitles: SideTitles(showTitles: false),
-            rightTitles: SideTitles(showTitles: false),
-            topTitles: SideTitles(
-                showTitles: true,
-                getTextStyles: (context, value) => TextStyle(
-                    color: Colors.black54,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14.sm),
-                getTitles: (double value) {
-                  switch (value.toInt()) {
-                    case 0:
-                      return 'Sun';
-                    case 1:
-                      return 'Mon';
-                    case 2:
-                      return 'Tue';
-                    case 3:
-                      return 'Wed';
-                    case 4:
-                      return 'Thu';
-                    case 5:
-                      return 'Fri';
-                    case 6:
-                      return 'Sat';
-                    default:
-                      return '';
-                  }
-                })),
-        barGroups: [
-          BarChartGroupData(x: 0, barRods: [
-            BarChartRodData(
-              y: 6000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-          BarChartGroupData(x: 1, barRods: [
-            BarChartRodData(
-              y: 9000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-          BarChartGroupData(x: 2, barRods: [
-            BarChartRodData(
-              y: 8000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-          BarChartGroupData(x: 3, barRods: [
-            BarChartRodData(
-              y: 5000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-          BarChartGroupData(x: 4, barRods: [
-            BarChartRodData(
-              y: 6000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-          BarChartGroupData(x: 5, barRods: [
-            BarChartRodData(
-              y: 4000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-          BarChartGroupData(x: 6, barRods: [
-            BarChartRodData(
-              y: 7000,
-              colors: [Colors.blueAccent],
-              width: 10,
-            ),
-          ]),
-        ]));
+    return Container(
+      margin: const EdgeInsets.only(top: 16),
+      height: 200,
+      child: BarChart(BarChartData(
+          alignment: BarChartAlignment.spaceAround,
+          maxY: 10000,
+          borderData: FlBorderData(show: false),
+          gridData: FlGridData(show: false),
+          titlesData: FlTitlesData(
+              bottomTitles: SideTitles(showTitles: false),
+              leftTitles: SideTitles(showTitles: false),
+              rightTitles: SideTitles(showTitles: false),
+              topTitles: SideTitles(
+                  showTitles: true,
+                  getTextStyles: (context, value) => TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.sp),
+                  getTitles: (double value) {
+                    switch (value.toInt()) {
+                      case 0:
+                        return 'Sun';
+                      case 1:
+                        return 'Mon';
+                      case 2:
+                        return 'Tue';
+                      case 3:
+                        return 'Wed';
+                      case 4:
+                        return 'Thu';
+                      case 5:
+                        return 'Fri';
+                      case 6:
+                        return 'Sat';
+                      default:
+                        return '';
+                    }
+                  })),
+          barGroups: [
+            BarChartGroupData(x: 0, barRods: [
+              BarChartRodData(
+                y: 6000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+            BarChartGroupData(x: 1, barRods: [
+              BarChartRodData(
+                y: 9000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+            BarChartGroupData(x: 2, barRods: [
+              BarChartRodData(
+                y: 8000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+            BarChartGroupData(x: 3, barRods: [
+              BarChartRodData(
+                y: 5000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+            BarChartGroupData(x: 4, barRods: [
+              BarChartRodData(
+                y: 6000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+            BarChartGroupData(x: 5, barRods: [
+              BarChartRodData(
+                y: 4000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+            BarChartGroupData(x: 6, barRods: [
+              BarChartRodData(
+                y: 7000,
+                colors: [Colors.blueAccent],
+                width: 10,
+              ),
+            ]),
+          ])),
+    );
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:finance_app/chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ionicons/ionicons.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,10 +9,60 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          SizedBox(height: 300, child: FinanceChart()),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  iconSize: 32.sm,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Ionicons.arrow_up,
+                    color: Colors.red,
+                  )),
+              IconButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  iconSize: 32.sm,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Ionicons.arrow_down,
+                    color: Colors.greenAccent,
+                  )),
+              IconButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  iconSize: 32.sm,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Ionicons.cash,
+                    color: Colors.amberAccent,
+                  )),
+              IconButton(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  iconSize: 32.sm,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Ionicons.cloud_upload,
+                    color: Colors.blue,
+                  )),
+            ],
+          ),
+          Text(
+            'Activities',
+            style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+          ),
+          const FinanceChart(),
         ],
       ),
     );
